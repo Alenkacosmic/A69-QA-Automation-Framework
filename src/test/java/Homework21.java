e1973242066987dc1d65c234b1d9df39839d4e05
Alenkacosmic <elena.capienko@gmail.com>
2025-04-02 22:19:14 -0400

    HW20 task update - added second test version for edit playlist name process

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -1,20 +1,29 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class Homework21 extends BaseTest{
-    @Test
-    public void renamePLaylist() {
-        String playlistName = "First Playlist Name";
-        String newPlaylistName = "Updated Playlist Name";
+    String playlistName = "First Playlist Name";
+    String newPlaylistName = "Updated Playlist Name";
 
+    @Test
+    public void renamePlaylist() {
         loginIntoApplication(validEmail, validPassword);
         createNewPlaylist(playlistName);
         verifySectionTitle(playlistName);
-        renamePlaylist(playlistName, newPlaylistName);
+        renamePlaylistWithDoubleClick(playlistName, newPlaylistName);
+        verifySectionTitle(newPlaylistName);
+        deleteEmptyPlaylist();
+    }
+
+    @Test
+    public void renamePlaylistThroughMenu() {
+        loginIntoApplication(validEmail, validPassword);
+        createNewPlaylist(playlistName);
+        verifySectionTitle(playlistName);
+        renamePlaylistUsingMenu(playlistName, newPlaylistName);
         verifySectionTitle(newPlaylistName);
         deleteEmptyPlaylist();
     }
